1ee8614917f7d8e9275629c217d07fc9001f0e6d
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2023-12-12 23:55:00 -0300

    first commit

---
+++ src/main/java/geolocalizacao/geolocalizacao/GeolocalizacaoApplication.java
@@ -0,0 +1,13 @@
+package geolocalizacao.geolocalizacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeolocalizacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeolocalizacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/geolocalizacao/geolocalizacao/GeolocalizacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package geolocalizacao.geolocalizacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeolocalizacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
